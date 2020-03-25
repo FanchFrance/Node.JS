43e5c7f996189715f5b7430a4218e07b15f3593c
François Yhuel <59474651+FanchFrance@users.noreply.github.com>
2020-03-25 16:23:25 +0100

    Create wilder.js

---
+++ wilder.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+const log = console.log;
+
+log(chalk.blue('Enzo') +('!'));
+log(chalk.green('Mathilde') +('!'));
+log(chalk.red('Thomas volcan') +('!'));
+log(chalk.yellow('christelle') +('!'));
